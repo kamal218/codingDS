9de8f2b7dc6483db582d259cb03972f66d897628
kamal <kamal@git.com>
2020-05-12 21:42:35 +0530

    update

--- DS AND ALGO/ARRAYS/basicfunc.java
+++ DS AND ALGO/ARRAYS/basicfunc.java
@@ -10,8 +10,10 @@ public class basicfunc {
 
     public static void solve() {
         basicf();
+        // LeetCode();
     }
 
+    /**********************************************************************************/
     public static void basicf() {
         // MIN_MAX
         /*
@@ -28,26 +30,19 @@ public class basicfunc {
 
         // Reverse array
         /*
-         * int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 }; revArray(arr); for (int ele : arr)
-         * System.out.print(ele + " ");
+         * int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 }; revArray(arr); display1D(arr);
          */
 
-        // Rotate Array
-        /*
-         * rotateArray(arr,3); for (int ele : arr) System.out.print(ele + " ");
-         */
+        // Matrix
 
-        // without space
+        int[][] mat = { { 1, 2, 3 }, { 2, 3, 4 }, { 4, 5, 6 } };
+        int[][] hmat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        // addTwoMatrix(mat, hmat);
+        // transposeMatrix(mat);
+        // multiplyMatrix(mat, hmat);
+        wavePrint(mat);
+        // display2D(matrix);
 
-        /*
-         * rotateArrayWithoutSpace(arr,3);for (int ele : arr) System.out.print(ele +
-         * " ");
-         */
-
-        // SEARCH IN ROTATEDARRAY
-        /*
-         * rotateArray(arr,3); return searchInRotatedArry(arr,4);
-         */
     }
 
     public static int[] min_max(int[] arr) {
@@ -77,12 +72,6 @@ public class basicfunc {
             si++;
             ei--;
         }
-    }
-
-    public static void swapf(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 
     public void rotateArray(int[] nums, int k) {
@@ -155,4 +144,190 @@ public class basicfunc {
         }
         return -1;
     }
+
+    public static void addTwoMatrix(int[][] mat1, int[][] mat2) {
+        int[][] ans = new int[mat1.length][mat1[0].length];
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[0].length; j++) {
+                ans[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+        display2D(ans);
+    }
+
+    public static void transposeMatrix(int[][] mat) {
+        // int[][] ans=new int[mat[0].length][mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (i <= j)
+                    swap2D(mat, i, j); // Extra space ans[j][i]=mat[i][j];
+            }
+        }
+        display2D(mat);
+    }
+
+    public static void multiplyMatrix(int[][] mat1, int[][] mat2) {
+        int r1 = mat1.length;
+        int r2 = mat2.length;
+        int c1 = mat1[0].length;
+        int c2 = mat2[0].length;
+        int sum = 0;
+        if (c2 != r1) {
+            System.out.println("\n" + " \t OOPS! Matrices cannot be multiplies");
+            return;
+        }
+        int[][] ans = new int[r1][c2];
+        for (int i = 0; i < ans.length; i++) {
+            sum = 0;
+            for (int j = 0; j < ans[0].length; j++) {
+                for (int k = 0; k < c1; k++) {
+                    sum += (mat1[i][k] * mat2[k][j]);
+                }
+                ans[i][j] = sum;
+            }
+        }
+        display2D(ans);
+    }
+
+    public static void wavePrint(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            if (i % 2 == 0) // if(i==0 || (i&(i-1))!=0) // more faster method to find %2 then previous
+            {
+                for (int j = 0; j < mat[0].length; j++)
+                    System.out.print(mat[i][j] + " ");
+            } else {
+                for (int j = mat[0].length - 1; j >= 0; j--) {
+                    System.out.print(mat[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /***************************************************************************/
+    public static void LeetCode() {
+        // int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        // Rotate Array
+        /*
+         * rotateArray(arr,3); for (int ele : arr) System.out.print(ele + " ");
+         */
+
+        // without space
+
+        /*
+         * rotateArrayWithoutSpace(arr,3);for (int ele : arr) System.out.print(ele +
+         * " ");
+         */
+
+        // SEARCH IN ROTATEDARRAY
+        /*
+         * rotateArray(arr,3); return searchInRotatedArry(arr,4);
+         */
+
+        // LEETCODE 1431
+        /*
+         * int[] candies={2,3,5,3,0}; ArrayList<Boolean> ans=kidsWithCandies(candies,3);
+         * for(Boolean ele:ans) { System.out.println(ele+" "); }
+         */
+        // LEETCODE 1313
+
+        /*
+         * int[] arr={1,2,3,4}; int[] ans=decompressRLElist(arr); display1D(ans);
+         */
+        // LEETCODE 1295
+        int[] nums={1,154,0,22,44,7893};
+        System.out.println(findNumbers(nums));
+    }
+
+    public static ArrayList<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> ans = new ArrayList<Boolean>();
+        int max_ = -Integer.MIN_VALUE;
+        for (int ele : candies) {
+            if (ele > max_)
+                max_ = ele;
+        }
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max_)
+                ans.add(true);
+            else
+                ans.add(false);
+        }
+        return ans;
+    }
+
+    public static int[] decompressRLElist(int[] nums) {
+        int size_ = 0;
+        for (int i = 0; i < nums.length; i += 2)
+            size_ += nums[i];
+        int[] ans = new int[size_];
+        int idx = 0;
+        for (int i = 0, j = 1; i < nums.length; i += 2, j += 2) {
+            while (nums[i] != 0) {
+                ans[idx] = nums[j];
+                nums[i]--;
+                idx++;
+            }
+        }
+        return ans;
+    }
+
+    public static int findNumbers(int[] nums) {
+        int ans = 0;
+        for (int ele : nums) {
+            if (digits(ele) % 2 == 0)
+                ans++;
+        }
+        return ans;
+    }
+
+    public static int digits(int n) {
+        int dig = 0;
+        while (n != 0) {
+            dig++;
+            n /= 10;
+        }
+        return dig;
+    }
+    // public int findNumbers(int[] nums) {
+    //     int ans=0;
+    //     for(int ele:nums)
+    //     {
+    //         if((ele>=10 && ele<=99) || (ele>=1000 && ele<=9999)  || ele==100000)
+    //             ans++;
+    //     }
+    //     return ans;
+    // }
+
+    // *****************************************************************//
+    // Helper functions
+
+    public static void display1D(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+
+    public static void display2D(int[][] mat) {
+        for (int[] submat : mat) {
+            for (int ele : submat) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void swapf(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void swap2D(int[][] mat, int i, int j) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
+    }
+    // *****************************************************************/
 }
