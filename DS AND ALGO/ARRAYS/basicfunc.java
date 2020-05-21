087315025dd7fd1b54ee272f675428980eb1ab9d
kamal <kamal@git.com>
2020-05-21 15:20:39 +0530

    MORE LEETCODE SOLUTIONS

--- DS AND ALGO/ARRAYS/basicfunc.java
+++ DS AND ALGO/ARRAYS/basicfunc.java
@@ -407,14 +407,41 @@ public class basicfunc {
          */
         // LEETCODE 922
         /*
-        int[] arr = { 4, 2, 3, 5 };
-        display1D(arr);
-        */
+         * int[] arr = { 4, 2, 3, 5 }; display1D(arr);
+         */
         // LEETCODE1
         /*
-        int[] nums={2,3,4};
-        display1D(twoSum(nums,6));
-        */
+         * int[] nums={2,3,4}; display1D(twoSum(nums,6));
+         */
+        // LEETCODE 1450
+        /*
+         * int[] startTime={1,2,3}; int[] endTime={10,5,12};
+         * System.out.println(busyStudent(startTime, endTime, 4));
+         */
+        // LEETCODE 1122
+        /*
+         * int[] arr1={2,3,1,3,2,4,6,7,9,2,19}; int[] arr2={2,1,4,3,9,6}
+         * display1D(relativeSortArray(arr1, arr2)); display1D(relativeSortArray2(arr1,
+         * arr2)); //faster the above method no need of sorting
+         */
+        // LEETCODE 1160
+        /*
+         * String[] words= {"cat","bt","hat","tree"}; String chars="atach";
+         * System.out.println(countCharacters(words, chars));
+         */
+        // LEETCODE 1200
+        /*
+         * int[] arr={40,11,26,27,-20}; displayArrayList2D(minimumAbsDifference(arr));
+         */
+        // LEETCODE 1394
+        /*
+         * int[] arr={2,2,2,3,3}; System.out.println(findLucky(arr));
+         * System.out.println(findLuckyNoHashMap(arr));
+         */
+        // LEETCODE 1413
+        int[] nums={-3,2,-3,4,2};
+        System.out.println(minStartValue(nums));
+
     }
 
     public static ArrayList<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
@@ -741,6 +768,147 @@ public class basicfunc {
         return ans;
     }
 
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] == queryTime || endTime[i] == queryTime
+                    || (startTime[i] < queryTime && endTime[i] > queryTime))
+                ans += 1;
+        }
+        return ans;
+    }
+
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr1) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        int[] ans = new int[arr1.length];
+        int idx = 0;
+        for (int ele : arr2) {
+            for (int i = 0; i < map.get(ele); i++)
+                ans[idx++] = ele;
+            map.remove(ele);
+        }
+        Arrays.sort(arr1);
+        for (int i = 0; i < arr1.length; i++) {
+            if (map.containsKey(arr1[i])) {
+                for (int j = 0; j < map.get(arr1[i]); j++) {
+                    ans[idx++] = arr1[i];
+                }
+                map.remove(arr1[i]);
+            }
+        }
+        return ans;
+    }
+
+    public static int[] relativeSortArray2(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr1) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        int[] ans = new int[arr1.length];
+        int idx = 0;
+        for (int ele : arr2) {
+            for (int i = 0; i < map.get(ele); i++)
+                ans[idx++] = ele;
+            map.remove(ele);
+        }
+        int[] arr = new int[ans.length - idx];
+        int idx1 = 0;
+        for (Integer ele : map.keySet()) {
+            for (int i = 0; i < map.get(ele); i++) {
+                arr[idx1++] = ele;
+            }
+        }
+        idx1 = 0;
+        Arrays.sort(arr);
+        for (int i = idx; i < ans.length; i++) {
+            ans[i] = arr[idx1++];
+        }
+        return ans;
+    }
+
+    public static int countCharacters(String[] words, String chars) {
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> temp = new HashMap<>();
+        for (int i = 0; i < chars.length(); i++) {
+            map.put(chars.charAt(i), map.getOrDefault(chars.charAt(i), 0) + 1);
+        }
+        int j = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (Character ch : map.keySet())
+                temp.put(ch, map.get(ch));
+            for (j = 0; j < words[i].length(); j++) {
+                if (temp.containsKey(words[i].charAt(j)) && temp.get(words[i].charAt(j)) != 0) {
+                    temp.put(words[i].charAt(j), temp.get(words[i].charAt(j)) - 1);
+                } else
+                    break;
+            }
+            if (j == words[i].length())
+                ans += words[i].length();
+        }
+        return ans;
+    }
+
+    public static ArrayList<ArrayList<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int min_ = 1452145;
+        for (int i = 0; i < arr.length - 1; i++) {
+            min_ = Math.min(min_, arr[i + 1] - arr[i]);
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] <= min_) {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(arr[i]);
+                list.add(arr[i + 1]);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+
+    public static int findLucky(int[] arr) {
+        int ans = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == key)
+                ans = Math.max(ans, key);
+        }
+        return ans;
+    }
+
+    public static int findLuckyNoHashMap(int[] arr) {
+        int ans = -1, ct = 1;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            ct = 1;
+            while (i + 1 < arr.length && arr[i + 1] == arr[i]) {
+                ct++;
+                i++;
+            }
+            if (ct == arr[i] && ct > ans)
+                ans = ct;
+
+        }
+        return ans;
+    }
+
+    public static int minStartValue(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        int ps = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ps += nums[i];
+            ans = Math.min(ps, ans);
+        }
+        return ans > 0 ? 1 : ((-ans) + 1);
+    }
+
     // ******************************// Helper
     // functions***********************************//
 
@@ -755,6 +923,22 @@ public class basicfunc {
         for (int[] submat : mat) {
             for (int ele : submat) {
                 System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void displayArrayList1D(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++)
+            System.out.print(list.get(i) + " ");
+        System.out.println();
+    }
+
+    public static void displayArrayList2D(ArrayList<ArrayList<Integer>> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length; j++) {
+                System.out.print(list.get(i).get(j) + " ");
             }
             System.out.println();
         }
