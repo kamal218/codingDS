b9f19a0cad1c250a4dffd4b53ecd92a61e42e0f5
kamal <kamal@git.com>
2020-07-30 16:38:05 +0530

    update

--- DS AND ALGO/TRIE/Trie.java
+++ DS AND ALGO/TRIE/Trie.java
@@ -1,54 +1,55 @@
-public class Trie{
+public class Trie {
 
-    public class Node{
-      int wordEnd=0;
-      Node[] children;
-      
-      public Node(){
-        this.children=new Node[26];
-        this.wordEnd=0;
-      }
+    public class Node {
+        int wordEnd = 0;
+        Node[] children;
+
+        public Node() {
+            this.children = new Node[26];
+            this.wordEnd = 0;
+        }
 
     }
-    Node root=new Node();
 
-    public void insert(String str){
-        Node curr=root;
-        if(curr==null)
-        curr=new Node();
-        for(int i=0;i<str.length();i++){
-            int idx=str.charAt(i)-'a';
-            if(curr.children[idx]==null)
-            curr.children[idx]=new Node();
-            curr=curr.children[idx];
+    Node root = new Node();
+
+    public void insert(String str) {
+        Node curr = root;
+        if (curr == null)
+            curr = new Node();
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            if (curr.children[idx] == null)
+                curr.children[idx] = new Node();
+            curr = curr.children[idx];
         }
         curr.wordEnd++;
     }
 
-    public int search(String str){
-        Node curr=root;
-        for(int i=0;i<str.length();i++){
-            int idx=str.charAt(i)-'a';
-            if(curr.children[idx]==null){
-            System.out.print("Word doesnot exist");
-            return -1;
+    public int search(String str) {
+        Node curr = root;
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                System.out.print("Word doesnot exist");
+                return -1;
             }
-            curr=curr.children[idx];
+            curr = curr.children[idx];
         }
-        if(curr.wordEnd==0){
+        if (curr.wordEnd == 0) {
             System.out.print("Word doesnot exist ");
-            return -1;  
+            return -1;
         }
         return curr.wordEnd;
     }
 
-    public boolean isPrefix(String str){
-        Node curr=root;
-        for(int i=0;i<str.length();i++){
-            int idx=str.charAt(i)-'a';
-            if(curr.children[idx]==null)
-            return false;
-            curr=curr.children[idx];
+    public boolean isPrefix(String str) {
+        Node curr = root;
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            if (curr.children[idx] == null)
+                return false;
+            curr = curr.children[idx];
         }
         return true;
     }
--- DS AND ALGO/TRIE/client.java
+++ DS AND ALGO/TRIE/client.java
@@ -1,11 +1,13 @@
-public class client{
-    public static void main(String[] args){
+public class client {
+    public static void main(String[] args) {
         Trie myDictionary = new Trie();
-        String[] words={"the","they","them","there","their","tree","tree","tree","tree","trie","make","made","free","fly","year","yes","yet"};
-        for(int i=0;i<words.length;i++){
+        String[] words = { "the", "they", "them", "there", "their", "tree", "tree", "tree", "tree", "trie", "make",
+                "made", "free", "fly", "year", "yes", "yet" };
+        for (int i = 0; i < words.length; i++) {
             myDictionary.insert(words[i]);
         }
+
         System.out.println(myDictionary.search("tree"));
-        System.out.print(myDictionary.search("ther")+"\n"+myDictionary.isPrefix("ther"));
+        System.out.print(myDictionary.search("ther") + "\n" + myDictionary.isPrefix("ther"));
     }
 }
